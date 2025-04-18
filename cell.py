ce462d9de7ac716a46d6ea1b650737ba5a92b174
mrdaner <lakhnodima@gmail.com>
2025-04-18 17:53:35 +0000

    class cell

---
+++ cell.py
@@ -0,0 +1,9 @@
+from graphics import Line, Point
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
