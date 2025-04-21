714c96eb2233f5cfc0998746c0262fc1b0d020de
mrdaner <lakhnodima@gmail.com>
2025-04-21 18:21:49 +0000

    cell

--- graphics.py
+++ graphics.py
@@ -35,9 +35,9 @@ class Point:
 
 class Line:
     def __init__(
-        self,
-        p1,
-        p2,
+            self,
+            p1,
+            p2,
     ):
         self.p1 = p1
         self.p2 = p2
